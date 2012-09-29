bc370eafade5e36997f636867c7bbfe97685e885
Peter Watts <pwattsmail@gmail.com>
2012-09-29 13:27:37 -0700

    bug fixes yo

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-var api_key = 'BYLBYN7O3QSEBROAR';
-var params = [], artists = [], songs = [];
+var api_key = 'BYLBYN7O3QSEBROAR', params = [], songs = [];
 $(".vevent .line-up ul li a").each(function(i,x){
 	if(params.length<5) {	// Max 5 artists supported
 		params.push('artist_id=songkick:artist:' + $(this).attr('href').split('/')[2].split('-')[0] + ($(this).parent().hasClass('headliner') ? "^2" : ""));
-		artists.push($(this).text());
 	}
 });
 if(params.length>0) {
@@ -12,9 +10,9 @@ if(params.length>0) {
 			songs.push(song.tracks[0].foreign_id.split(':')[2]);
 		});
 		if(songs.length>0) {
-			var playlist = 'spotify:trackset:'+artists.join(' + ')+':'+songs.join(',');
-			var player = '<iframe style="margin-top:10px" src="https://embed.spotify.com/?uri='+playlist+'" width="260" height="340" frameborder="0" allowtransparency="true"></iframe>';
-			$(".secondary.col").prepend('<div id="pregig" style="padding:16px 20px 15px 20px"><h3>PreGig Playlist</h3></div>');
+			var playlist = 'spotify:trackset:PreGig Playlist:'+songs.join(',');
+			var player = '<iframe style="padding-top:10px;border-top: 1px solid #fff;" src="https://embed.spotify.com/?uri='+playlist+'" width="260" height="340" frameborder="0" allowtransparency="true"></iframe>';
+			$(".secondary.col").prepend('<div id="pregig" style="padding: 16px 16px 15px 20px"><h3>PreGig Playlist</h3></div>');
 			$("#pregig").append(player);
 		}		
 	});
